d04ef2d7b907461ba72972bf8fe8e89c70566db1
WCharacter <mitayletsplay@gmail.com>
2021-04-10 18:51:18 +0300

    fixed bug when image wasn't remove after switching to another video, added buttons for all resolutions

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ Very simple extension for chrome to get the highest resolution thumbnail from a 
 # What it does
 
 It injects javascript code into youtube page and adds button into player.
-Thumbnail will be added into info area below the player. 
+After clicking this button thumbnail will be added into info area below the player. 
--- js/thumnailGrabber.js
+++ js/thumnailGrabber.js
@@ -1,7 +1,7 @@
 var addButton = function(){
-    a = document.getElementsByClassName("ytp-right-controls")[0];
-    b = document.createElement("button");
+    controls = document.getElementsByClassName("ytp-right-controls")[0];  
 
+    b = document.createElement("button");
     b.classList.add("playerButton");
     b.classList.add("ytp-button");
     b.id = "grabThumbnail";
@@ -13,20 +13,123 @@ var addButton = function(){
     img.classList.add("thumbGrabImage");
 
     b.appendChild(img);
-    b.addEventListener("click", ()=>{
-        if(document.getElementById("thumbnailImg") === null) {
-            let id = document.URL.split('=')[1];
-            thumbnailImg = document.createElement("img");
-            thumbnailImg.src = `https://img.youtube.com/vi/${id}/maxresdefault.jpg`;
-            thumbnailImg.id = "thumbnailImg";
+    b.addEventListener("click", () => {
+        thumbBlock = document.getElementById("thumbnailImages");
+        if(thumbBlock === null) {
+            let id = document.URL.split('=')[1].substr(0,11);
+
+            thumbBlock = document.createElement("div");
+            thumbBlock.id = "thumbnailImages";
+            
+            maxRes = document.createElement("img");
+            maxRes.src = `https://img.youtube.com/vi/${id}/maxresdefault.jpg`;
+            maxRes.id = "maxrRes";
+            maxRes.style = "display: block;";
+
+            defaultRes = document.createElement("img");
+            defaultRes.src = `https://img.youtube.com/vi/${id}/default.jpg`;
+            defaultRes.id = "defaultRes";
+            defaultRes.style = "display: none;";
+
+            mediumRes = document.createElement("img");
+            mediumRes.src = `https://img.youtube.com/vi/${id}/mqdefault.jpg`;
+            mediumRes.id = "mediumRes";
+            mediumRes.style = "display: none;";
+            
+            standardRes = document.createElement("img");
+            standardRes.src = `https://img.youtube.com/vi/${id}/sddefault.jpg`;
+            standardRes.id = "mediumRes";
+            standardRes.style = "display: none;";
+
+            highRes = document.createElement("img");
+            highRes.src = `https://img.youtube.com/vi/${id}/hqdefault.jpg`;
+            highRes.id = "highRes";
+            highRes.style = "display: none;";
+
+            images = [maxRes, defaultRes, mediumRes, standardRes, highRes];
+
+            maxResBtn = document.createElement("button");
+            maxResBtn.id = "maxResBtn";
+            maxResBtn.innerText = "Max resolution";
+            maxResBtn.addEventListener("click", () => {
+                for(var i = 0; i < images.length; i++){
+                    images[i].style = "display: none;";
+                }
+                maxRes.style = "display: block;";
+            });
+
+            defaultResBtn = document.createElement("button");  
+            defaultResBtn.id = "defaultResBtn";
+            defaultResBtn.innerText = "Default resolution";
+            defaultResBtn.addEventListener("click", () => {
+                for(var i = 0; i < images.length; i++){
+                    images[i].style = "display: none;";
+                }
+                defaultRes.style = "display: block;";
+            });
+            
+            mediumResBtn = document.createElement("button");
+            mediumResBtn.id = "mediumResBtn";
+            mediumResBtn.innerText = "Medium resolution";
+            mediumResBtn.addEventListener("click", () => {
+                for(var i = 0; i < images.length; i++){
+                    images[i].style = "display: none;";
+                }
+                mediumRes.style = "display: block;";
+            });
+
+            standardResBtn = document.createElement("button");
+            standardResBtn.id = "standardResBtn";
+            standardResBtn.innerText = "Standard resolution";
+            standardResBtn.addEventListener("click", () => {
+                for(var i = 0; i < images.length; i++){
+                    images[i].style = "display: none;";
+                }
+                standardRes.style = "display: block;";
+            });
+
+            highResBtn = document.createElement("button");
+            highResBtn.id = "highResBtn";
+            highResBtn.innerText = "High resolution";
+            highResBtn.addEventListener("click", () => {
+                for(var i = 0; i < images.length; i++){
+                    images[i].style = "display: none;";
+                }
+                highRes.style = "display: block;";
+            });
+            
+            thumbBlock.appendChild(maxResBtn);
+            thumbBlock.appendChild(defaultResBtn);
+            thumbBlock.appendChild(mediumResBtn);
+            thumbBlock.appendChild(standardResBtn);
+            thumbBlock.appendChild(highResBtn);
+
+            for(var i = 0; i < images.length; i++){
+                thumbBlock.appendChild(images[i]);
+            }
+
             infBlock = document.getElementById("info");
-            infBlock.appendChild(thumbnailImg);
+            infBlock.appendChild(thumbBlock);
         }
         else {
-            thumbnailImg = document.getElementById("thumbnailImg");
-            thumbnailImg.remove();
+            thumbBlock.remove();
         }
     });
 
-    a.insertBefore(b, a.firstChild);
-}();
+    controls.insertBefore(b, controls.firstChild);
+}
+
+// waiting some time before execution
+setTimeout(addButton, 500);
+
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if (request.action === "removeBlock") {
+            thumbBlock = document.getElementById("thumbnailImages");
+            if(thumbBlock !== null) {
+                thumbBlock.remove();
+            }
+        }
+    }
+);
