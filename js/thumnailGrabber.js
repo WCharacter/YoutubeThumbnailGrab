c319e16ab15e24fc0d4c543dd8858a2ad4be19b8
WCharacter <mitayletsplay@gmail.com>
2021-04-11 01:09:26 +0300

    code refactoring

--- js/thumnailGrabber.js
+++ js/thumnailGrabber.js
@@ -1,6 +1,28 @@
+var createButton = function(id, txt, obj, imgs){
+	btn = document.createElement("button");
+    btn.id = id;
+    btn.innerText = txt;
+    btn.addEventListener("click", () => {
+        for(var i = 0; i < imgs.length; i++){
+            imgs[i].style = "display: none;";
+        }
+        obj.style = "display: block;";
+    });
+	return btn;
+}
+
+var createImg = function(imgId, id, imgType, display){
+	img = document.createElement("img");
+    img.src = `https://img.youtube.com/vi/${id}/${imgType}`;;
+    img.id = imgId;
+    img.style = `display: ${display};`;	
+	return img;
+}
+
 var addButton = function(){
     controls = document.getElementsByClassName("ytp-right-controls")[0];  
-
+	if(controls === undefined) return;
+	
     b = document.createElement("button");
     b.classList.add("playerButton");
     b.classList.add("ytp-button");
@@ -21,82 +43,19 @@ var addButton = function(){
             thumbBlock = document.createElement("div");
             thumbBlock.id = "thumbnailImages";
             
-            maxRes = document.createElement("img");
-            maxRes.src = `https://img.youtube.com/vi/${id}/maxresdefault.jpg`;
-            maxRes.id = "maxrRes";
-            maxRes.style = "display: block;";
-
-            defaultRes = document.createElement("img");
-            defaultRes.src = `https://img.youtube.com/vi/${id}/default.jpg`;
-            defaultRes.id = "defaultRes";
-            defaultRes.style = "display: none;";
-
-            mediumRes = document.createElement("img");
-            mediumRes.src = `https://img.youtube.com/vi/${id}/mqdefault.jpg`;
-            mediumRes.id = "mediumRes";
-            mediumRes.style = "display: none;";
-            
-            standardRes = document.createElement("img");
-            standardRes.src = `https://img.youtube.com/vi/${id}/sddefault.jpg`;
-            standardRes.id = "mediumRes";
-            standardRes.style = "display: none;";
-
-            highRes = document.createElement("img");
-            highRes.src = `https://img.youtube.com/vi/${id}/hqdefault.jpg`;
-            highRes.id = "highRes";
-            highRes.style = "display: none;";
+            maxRes = createImg("maxRes", id, "maxresdefault.jpg", "block");
+            defaultRes = createImg("defaultRes", id, "default.jpg", "none");
+            mediumRes = createImg("mediumRes", id, "mqdefault.jpg", "none");
+            standardRes = createImg("standardRes", id, "sddefault.jpg", "none");
+            highRes = createImg("highRes", id, "hqdefault.jpg", "none");
 
             images = [maxRes, defaultRes, mediumRes, standardRes, highRes];
 
-            maxResBtn = document.createElement("button");
-            maxResBtn.id = "maxResBtn";
-            maxResBtn.innerText = "Max resolution";
-            maxResBtn.addEventListener("click", () => {
-                for(var i = 0; i < images.length; i++){
-                    images[i].style = "display: none;";
-                }
-                maxRes.style = "display: block;";
-            });
-
-            defaultResBtn = document.createElement("button");  
-            defaultResBtn.id = "defaultResBtn";
-            defaultResBtn.innerText = "Default resolution";
-            defaultResBtn.addEventListener("click", () => {
-                for(var i = 0; i < images.length; i++){
-                    images[i].style = "display: none;";
-                }
-                defaultRes.style = "display: block;";
-            });
-            
-            mediumResBtn = document.createElement("button");
-            mediumResBtn.id = "mediumResBtn";
-            mediumResBtn.innerText = "Medium resolution";
-            mediumResBtn.addEventListener("click", () => {
-                for(var i = 0; i < images.length; i++){
-                    images[i].style = "display: none;";
-                }
-                mediumRes.style = "display: block;";
-            });
-
-            standardResBtn = document.createElement("button");
-            standardResBtn.id = "standardResBtn";
-            standardResBtn.innerText = "Standard resolution";
-            standardResBtn.addEventListener("click", () => {
-                for(var i = 0; i < images.length; i++){
-                    images[i].style = "display: none;";
-                }
-                standardRes.style = "display: block;";
-            });
-
-            highResBtn = document.createElement("button");
-            highResBtn.id = "highResBtn";
-            highResBtn.innerText = "High resolution";
-            highResBtn.addEventListener("click", () => {
-                for(var i = 0; i < images.length; i++){
-                    images[i].style = "display: none;";
-                }
-                highRes.style = "display: block;";
-            });
+            maxResBtn = createButton("maxResBtn", "Max resolution", maxRes, images);
+            defaultResBtn = createButton("defaultResBtn", "Default resolution", defaultRes, images);
+            mediumResBtn = createButton("mediumResBtn", "Medium resolution", mediumRes, images);
+            standardResBtn = createButton("standardResBtn", "Standard resolution", standardRes, images);
+            highResBtn = createButton("highResBtn", "High resolution", highRes, images);           
             
             thumbBlock.appendChild(maxResBtn);
             thumbBlock.appendChild(defaultResBtn);
